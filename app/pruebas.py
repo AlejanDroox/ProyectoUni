fbebd172c54a815b1aa9bc06466ff80bc096364e
Alejandroox <darmdz23@gmail.com>
2024-06-25 14:43:51 -0400

    Revert "intentando conectar el inventario a bd"
    This reverts commit 572eb2e1ccbfa398fd3474f40a2c57b896aad30e.

--- app/pruebas.py
+++
@@ -1,6 +0,0 @@
-from db.crud_productos import ControlProductos, Producto
-from db.db_connector import DbConnector
-from utils.globals import CONFIG
-conx = DbConnector(CONFIG)
-crtl = ControlProductos(conx)
-crtl.devolver_productor(Producto)
